d5d6b42781020968fa566446f6613b01c1f8930c
Sebastian Navarro <sebastian.navarro@mercadolibre.com>
2020-09-20 20:16:40 -0300

    Web server first commit

---
+++ 07-WebServer/main.go
@@ -0,0 +1,70 @@
+// Server1 is a minimal "echo" server.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// --------------------------------------------------------------------Server #1--------------------------------------------------------------------
+// func main() {
+// 	http.HandleFunc("/data/", handler) // each request calls handler
+// 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+// }
+
+// // handler echoes the Path component of the requested URL.
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "URL.Path = %q \n sebas \n sebas \n sebas \n\n\n %q", r.URL.Path, r.URL.Host)
+// }
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------Server #2--------------------------------------------------------------------
+// var mu sync.Mutex
+// var count int
+
+// func main() {
+// 	http.HandleFunc("/", handler)
+// 	http.HandleFunc("/count", counter)
+// 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+// }
+
+// // handler echoes the Path component of the requested URL.
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	mu.Lock()
+// 	count++
+// 	mu.Unlock()
+// 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+// }
+
+// // counter echoes the number of calls so far.
+// func counter(w http.ResponseWriter, r *http.Request) {
+// 	mu.Lock()
+// 	fmt.Fprintf(w, "Count %d\n", count)
+// 	mu.Unlock()
+// }
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------Server #3--------------------------------------------------------------------
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echoes the HTTP request.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
